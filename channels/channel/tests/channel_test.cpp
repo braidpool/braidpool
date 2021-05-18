190361d27f5fac3cb66bc62ae7f3930979b04b40
wholooks <wholooks@users.noreply.github.com>
2021-05-18 18:40:37 +0200

    Setup testing from cmake with a dummy test

---
+++ channels/channel/tests/channel_test.cpp
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2021 d11dpool developers (see AUTHORS)
+ *
+ * This file is part of d11dpool.
+ *
+ * d11dpool is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * d11dpool is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with d11dpool.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#define BOOST_TEST_MODULE boost_test_macro_overview
+#include <boost/test/included/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_CASE( test_macro_overview )
+{
+  namespace tt = boost::test_tools;
+  int a = 1;
+  int b = 2;
+  BOOST_TEST(a == b - 1);
+  // BOOST_TEST(a + 1 < b);
+  // BOOST_TEST(b -1 > a, a << " < " << b - 1 << " does not hold");
+  // BOOST_TEST(a == b, tt::bitwise());
+  // BOOST_TEST(a + 0.1 == b - 0.8, tt::tolerance(0.01));
+}
