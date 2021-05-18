59759ba7fc959f774672579a56f8e523169d2438
wholooks <wholooks@users.noreply.github.com>
2021-05-18 21:20:45 +0200

    Add tests/main.cpp for channel library

--- channels/channel/tests/main.cpp
+++ channels/channel/tests/main.cpp
@@ -17,17 +17,5 @@
  * along with d11dpool.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#define BOOST_TEST_MODULE channel_tests
 #include <boost/test/unit_test.hpp>
-
-
-BOOST_AUTO_TEST_CASE( test_macro_overview )
-{
-  namespace tt = boost::test_tools;
-  int a = 1;
-  int b = 2;
-  BOOST_TEST(a == b - 1);
-  // BOOST_TEST(a + 1 < b);
-  // BOOST_TEST(b -1 > a, a << " < " << b - 1 << " does not hold");
-  // BOOST_TEST(a == b, tt::bitwise());
-  // BOOST_TEST(a + 0.1 == b - 0.8, tt::tolerance(0.01));
-}
