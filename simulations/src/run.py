6616069516e763e8956dc41531184c0a42c39f41
wholooks <wholooks@users.noreply.github.com>
2021-06-21 22:43:10 +0200

    Streamline cache

--- simulations/src/run.py
+++ simulations/src/run.py
@@ -7,22 +7,28 @@ from node import Node
 RANDOM_SEED = 42
 SIM_TIME = 10_000
 
-# Setup and start the simulation
-print('Process communication')
-random.seed(RANDOM_SEED)
-env = simpy.Environment()
 
-node_a = Node(name='a', env=env)
-node_b = Node(name='b', env=env)
-node_c = Node(name='c', env=env)
+def run():
+    # Setup and start the simulation
+    print('Process communication')
+    random.seed(RANDOM_SEED)
+    env = simpy.Environment()
 
-node_a.add_neighbour(node_b)
-node_b.add_neighbour(node_a)
-node_b.add_neighbour(node_c)
-node_c.add_neighbour(node_b)
-node_a.start()
-node_b.start()
-node_c.start()
+    node_a = Node(name='a', env=env)
+    node_b = Node(name='b', env=env)
+    node_c = Node(name='c', env=env)
 
-print('\nOne-to-many pipe communication\n')
-env.run(until=SIM_TIME)
+    node_a.add_neighbour(node_b)
+    node_b.add_neighbour(node_a)
+    node_b.add_neighbour(node_c)
+    node_c.add_neighbour(node_b)
+    node_a.start()
+    node_b.start()
+    node_c.start()
+
+    print('\nP2P broadcast communication\n')
+    env.run(until=SIM_TIME)
+
+
+if __name__ == '__main__':
+    run()
