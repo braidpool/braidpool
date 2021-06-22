9a4cfbbdc5abcc9a882fd8b629caaf9dc376f1d8
wholooks <wholooks@users.noreply.github.com>
2021-06-22 23:38:08 +0200

    Add test for broadcast pipe

---
+++ simulations/tests/broadcast_pipe_test.py
@@ -0,0 +1,21 @@
+import simpy
+
+from broadcast_pipe import BroadcastPipe
+
+
+class TestBroadcastPipe():
+
+    def receive_from_fifo_pipe(self, env, pipe):
+        last_received = 0
+        while True:
+            msg = yield env.process(pipe.get())
+            assert msg == last_received + 1
+            last_received += 1
+
+    def test_broadcast_pipe_is_fifo(self):
+        env = simpy.Environment()
+        pipe = BroadcastPipe(env)
+        pipe.put(1)
+        pipe.put(2)
+        pipe.put(3)
+        self.receive_from_fifo_pipe(env, pipe)
