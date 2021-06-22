002b9b9f26ed10992f99aa932b07c077c73e2bc1
wholooks <wholooks@users.noreply.github.com>
2021-06-22 11:05:23 +0200

    Support forwarding; add share and message classes

--- simulations/src/node.py
+++ simulations/src/node.py
@@ -18,6 +18,7 @@ import simpy
 
 from broadcast_pipe import BroadcastPipe
 from dag import DAG
+from message import Message
 from share import Share
 
 SHARE_PERIOD = 1000
@@ -52,7 +53,7 @@ class Node:
     def add_in_pipe(self, pipe):
         self.in_pipes.append(pipe)
 
-    def send(self):
+    def generate_shares(self):
         """Process to generate shares at random intervals."""
         while True:
             # wait for next share
@@ -65,9 +66,15 @@ class Node:
             # in the pipe first and then message_consumer gets from pipe,
             # the event.triggered will be True in the other order it will be
             # False
-            msg = Share(source=self.name, heads=self.heads(), env=self.env)
-            print(f's e: {self.env.now} n: {self.name} {msg}')
-            self.out_pipe.put(msg)
+            share = Share(source=self.name, heads=self.heads(), env=self.env)
+            msg = Message(share=share)
+            self.add_to_dag(msg)
+            self.send(msg)
+
+    def send(self, msg, forward=False):
+        _type = 's' if not forward else 'f'
+        print(f'{_type} e: {self.env.now} n: {self.name} {msg}')
+        self.out_pipe.put(msg)
 
     def receive(self):
         """A process which consumes messages."""
@@ -77,9 +84,30 @@ class Node:
 
             for _, msg in received_messages.items():
                 print(f'r e: {self.env.now} n: {self.name} {msg}')
-                yield self.env.timeout(self.message_processing_time(msg))
+                self.env.process(self.handle_receive(msg))
+
+    def forward(self, msg):
+        msg.decrement_count()
+        self.send(msg, forward=True)
+
+    def should_forward(self, msg):
+        return msg.should_forward() and not self.dag.has(msg.share.hash)
+
+    def add_to_dag(self, msg):
+        self.dag.add_edges(msg.share.hash, msg.share.heads)
+
+    def handle_receive(self, msg):
+        while True:
+            yield self.env.timeout(self.message_processing_time(msg))
+            # If receieved first time insert into dag
+            if self.should_forward(msg):
+                self.forward(msg)
+            # add to dag after should_forward check has been carried out
+            self.add_to_dag(msg)
+            # check counter and if required send again
+            # detect any missing shares and send request for receiving missing shares
 
     def start(self):
         print(f'{self.name} starting...')
         self.env.process(self.receive())
-        self.env.process(self.send())
+        self.env.process(self.generate_shares())
--- simulations/src/share.py
+++ simulations/src/share.py
@@ -1,9 +1,17 @@
+from hashlib import sha256
+
 
 class Share:
     def __init__(self, *, source, heads, env):
         self.timestamp = env.now
         self.source = source
         self.heads = heads
+        self.hash = self.get_hash()
+
+    def get_hash(self):
+        joined_heads = "".join(self.heads)
+        return sha256(f'{self.timestamp}{self.source}{joined_heads}'.
+                      encode()).hexdigest()
 
     def __repr__(self):
-        return f't: {self.timestamp} s: {self.source} h: {self.heads}'
+        return f't: {self.timestamp} s: {self.source} {self.hash}'
