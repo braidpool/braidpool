2aab39dc3dd0d78f942c804a2a23282628ccd9aa
wholooks <wholooks@users.noreply.github.com>
2021-07-15 12:31:37 +0200

    Use exponential random for share generation

--- simulations/src/run.py
+++ simulations/src/run.py
@@ -11,34 +11,42 @@ from topology import Topology
 
 def run(*, num_nodes, num_neighbours):
     # Setup and start the simulation
-    logging.basicConfig(format='%(levelname)s:%(message)s',
-                        level=config['logging']['level'])
+    logging.basicConfig(
+        format="%(levelname)s:%(message)s", level=config["logging"]["level"]
+    )
 
-    logging.info('Process communication')
-    random.seed(config['simulation']['random_seed'])
+    logging.info("Process communication")
+    random.seed(config["simulation"]["random_seed"])
     env = simpy.Environment()
 
-    network = Topology(env=env, num_nodes=5, num_neighbours=4)
+    network = Topology(env=env, num_nodes=num_nodes, num_neighbours=num_neighbours)
     for node in network.nodes.values():
         node.start()
 
-    logging.info('\nP2P broadcast communication\n')
-    env.run(until=config['simulation']['run_time'])
+    logging.info("\nP2P broadcast communication\n")
+    env.run(until=config["simulation"]["run_time"])
 
     for node in network.nodes.values():
-        logging.info(f'At {node.name}')
         logging.debug(list(nx.lexicographical_topological_sort(node.dag)))
         logging.debug(node.dag.edges())
-        if config.getboolean('simulation', 'save_dot'):
+        if config.getboolean("simulation", "save_dot"):
             g = nx.nx_agraph.to_agraph(node.dag)
             g.layout()
-            g.draw(f'/tmp/node_{node.name}.png', prog='dot')
+            g.draw(f"/tmp/node_{node.name}.png", prog="dot")
+        logging.info(
+            f"node: {node.name} sent: {len(node.shares_sent)} ({node.num_blocks}), "
+            f"not rewarded: {len(node.shares_not_rewarded)}"
+        )
+        if node.shares_sent:
+            logging.info(f" %age not rewarded {len(node.shares_not_rewarded)/len(node.shares_sent) * 100}")
         logging.info(node.shares_not_rewarded)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_nodes', '-n', type=int, help='Number of nodes')
-    parser.add_argument('--num_neighbours', '-d', type=int, help='Number of neighbors per node')
+    parser.add_argument("--num_nodes", "-n", type=int, help="Number of nodes")
+    parser.add_argument(
+        "--num_neighbours", "-d", type=int, help="Number of neighbors per node"
+    )
     args = parser.parse_args()
     run(num_nodes=args.num_nodes, num_neighbours=args.num_neighbours)
