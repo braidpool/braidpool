bf2c16bfd68626e8a8e6596a671e64cb0bd8435e
pool2win <pool2win@users.noreply.github.com>
2021-11-16 07:11:30 +0100

    Switch to googletest

--- pool/tests/work_test.cpp
+++ pool/tests/work_test.cpp
@@ -18,23 +18,21 @@
  */
 
 #include "work.hpp"
-#include <boost/test/unit_test.hpp>
+#include <gtest/gtest.h>
 #include <msgpack.hpp>
 
 using namespace bp;
 
-BOOST_AUTO_TEST_SUITE(pool_tests)
-
-BOOST_AUTO_TEST_CASE(work_test__constructor__returns_work)
+TEST(WORK_TEST, CONSTRUCTOR__RETURNS_WORK)
 {
     hash_digest previous_block = hash_literal(
         "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f");
     bp::work instance(
         10, std::move(previous_block), 1000000000, "some_coinbase", {});
-    BOOST_CHECK(instance.version() == 10);
+    EXPECT_EQ(instance.version(), 10);
 }
 
-BOOST_AUTO_TEST_CASE(work_test__serialization__should_deserialize)
+TEST(WORK_TEST, SERIALIZATION__SHOULD_DESERIALIZE)
 {
     hash_digest previous_block = hash_literal(
         "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f");
@@ -50,10 +48,8 @@ BOOST_AUTO_TEST_CASE(work_test__serialization__should_deserialize)
     bp::work deserialized_work;
     obj.convert(deserialized_work);
 
-    BOOST_CHECK(deserialized_work.version() == 10);
-    BOOST_CHECK(deserialized_work.previous_block_hash() == previous_block);
-    BOOST_CHECK(deserialized_work.difficulty() == 1000000000);
-    BOOST_CHECK(deserialized_work.coinbase() == "some_coinbase");
+    EXPECT_EQ(deserialized_work.version(), 10);
+    EXPECT_EQ(deserialized_work.previous_block_hash(), previous_block);
+    EXPECT_EQ(deserialized_work.difficulty(), 1000000000);
+    EXPECT_EQ(deserialized_work.coinbase(), "some_coinbase");
 }
-
-BOOST_AUTO_TEST_SUITE_END()
