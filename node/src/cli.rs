8b50445e18b43295bb292f07dda778cfd517a231
Bob McElrath <bob_git@mcelrath.org>
2023-09-11 11:33:02 -0400

    Make `cargo fmt` happy

--- node/src/cli.rs
+++ node/src/cli.rs
@@ -4,15 +4,15 @@ use std::path::PathBuf;
 #[derive(Parser, Debug)]
 pub struct Cli {
     /// Braid data directory
-    #[arg(long, default_value="~/.braidpool/")]
+    #[arg(long, default_value = "~/.braidpool/")]
     pub datadir: PathBuf,
 
     /// Bind to a given address and always listen on it
-    #[arg(long, default_value="0.0.0.0:25188")]
+    #[arg(long, default_value = "0.0.0.0:25188")]
     pub bind: String,
 
     /// Add a node to connect to and attempt to keep the connection open. This option can be
     /// specified multiple times
     #[arg(long)]
-    pub addnode: Option<Vec<String>>
+    pub addnode: Option<Vec<String>>,
 }
