1244cb4ef07ccf240bff454aa0d6054a7c8f9cea
Bernardo A. Rodrigues <bernardoaraujor@gmail.com>
2023-11-08 20:40:48 -0300

    check if rpc is alive

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -1,3 +1,5 @@
+use bitcoincore_rpc::RpcApi;
+
 pub fn setup(
     bitcoin: String,
     rpc_port: u16,
@@ -5,8 +7,13 @@ pub fn setup(
     rpc_pass: String,
 ) -> Result<bitcoincore_rpc::Client, bitcoincore_rpc::Error> {
     let rpc_url = format!("{}:{}", bitcoin, rpc_port);
-    bitcoincore_rpc::Client::new(
+    let rpc = bitcoincore_rpc::Client::new(
         &rpc_url,
         bitcoincore_rpc::Auth::UserPass(rpc_user, rpc_pass),
-    )
+    )?;
+
+    // check if rpc is alive
+    rpc.get_blockchain_info()?;
+
+    Ok(rpc)
 }
