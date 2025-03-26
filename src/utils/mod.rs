3a1016480cdd3f14da060a67a7bfdaed87583ddb
Calisto Abel Mathias <calisto.mathias.25@gmail.com>
2025-03-26 13:50:09 +0530

    Implement From and Into traits for Hash and BigUint
    - Implemented From<BigUint> for Hash to convert a BigUint into a big-endian representation of a Hash.
    - Implemented Into<BigUint> for Hash to convert a Hash back into a BigUint.
    - Ensured proper handling of byte array resizing and copying for accurate conversion.
    
    Signed-off-by: Calisto Abel Mathias <calisto.mathias.25@gmail.com>

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,8 +1,31 @@
 pub mod bitcoin;
 
+// Standard Imports
+use num_bigint::BigUint;
+
 // TODO: Implement the various traits for implicit conversions!
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
-pub struct Hash(pub [u64; 4]);
+pub struct Hash(pub [u8; 32]);
+
+impl From<BigUint> for Hash {
+    fn from(value: BigUint) -> Self {
+        let mut bytes = value.to_bytes_be();
+        bytes.resize(32, 0);
+
+        let mut array = [0u8; 32];
+        for i in 0..32 {
+            array[i] = bytes[i];
+        };
+
+        Hash(array)
+    }
+}
+
+impl Into<BigUint> for Hash {
+    fn into(self) -> BigUint {
+        BigUint::from_bytes_be(&self.0)
+    }
+}
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct Byte(pub u8);
