6a051d6c12999f970dab5a11388437f4926dd751
Calisto Abel Mathias <calisto.mathias.25@gmail.com>
2025-03-26 14:00:07 +0530

    Implement From trait for Hash from hex strings
    - Implemented From<String> for Hash to convert a hex string starting with 0x into a [u8; 32] array in big-endian format.
    - Implemented From<&str> for Hash to handle string slices representing hex strings.
    - Used hex::decode to parse hex strings and handle invalid hex strings with expect.
    
    Signed-off-by: Calisto Abel Mathias <calisto.mathias.25@gmail.com>

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,6 +2,7 @@ pub mod bitcoin;
 
 // Standard Imports
 use num_bigint::BigUint;
+use hex;
 
 // TODO: Implement the various traits for implicit conversions!
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
@@ -24,6 +25,27 @@ impl From<BigUint> for Hash {
 impl Into<BigUint> for Hash {
     fn into(self) -> BigUint {
         BigUint::from_bytes_be(&self.0)
+    }
+}
+
+impl From<String> for Hash {
+    fn from(hex: String) -> Self {
+        Hash::from(hex.as_str())
+    }
+}
+
+impl From<&str> for Hash {
+    fn from(hex: &str) -> Self {
+        let hex = hex.trim_start_matches("0x");
+        let mut bytes = hex::decode(hex).expect("Invalid hex string");
+        bytes.resize(32, 0);
+
+        let mut array = [0u8; 32];
+        for i in 0..32 {
+            array[i] = bytes[i];
+        };
+
+        Hash(array)
     }
 }
 
