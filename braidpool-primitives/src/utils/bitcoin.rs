8d52bf326d270b6b324229ad8a7d54e875c0cee0
Sansh2356 <sansh2356@gmail.com>
2025-04-05 12:30:34 +0000

    Changing the test utility functions actioning

--- braidpool-primitives/src/utils/bitcoin.rs
+++ braidpool-primitives/src/utils/bitcoin.rs
@@ -1,9 +1,13 @@
 // Standard Imports
+use bitcoin::merkle_tree::MerkleNode;
 use std::cell::Cell;
-
 // Primitives Imports
-use bitcoin::hashes::Sha256d;
-use bitcoin::{BlockHash, BlockHeader, BlockTime, BlockVersion, CompactTarget, TxMerkleNode, Txid};
+use bitcoin::secp256k1::hashes::hex::DisplayHex;
+use bitcoin::{
+    BlockHash, BlockHeader, BlockTime, BlockVersion, CompactTarget, Transaction, TxMerkleNode, Txid,
+};
+
+use crate::utils::test_utils::{hash_data, reverse_hex_byte_order};
 
 // Internal Type Definitions for Clarity
 type MerkleRoot = TxMerkleNode;
@@ -15,18 +19,44 @@ pub struct MerklePathProof {
 }
 
 impl MerklePathProof {
-    pub fn calculate_corresponding_merkle_root(&self) -> MerkleRoot {
-        let mut current_hash = self.transaction_hash.as_byte_array().clone();
-        for merkle_node in &self.merkle_path {
-            let merkle_node_as_bytes = merkle_node.as_byte_array();
-
-            let mut concatenated_hashes = Vec::new();
-            concatenated_hashes.extend_from_slice(&current_hash);
-            concatenated_hashes.extend_from_slice(merkle_node_as_bytes);
-
-            current_hash = Sha256d::hash(&concatenated_hashes).as_byte_array().clone();
+    pub fn calculate_corresponding_merkle_root(
+        &self,
+        testing_option: Option<(String, Vec<Transaction>)>,
+    ) -> MerkleRoot {
+        if testing_option.is_some() == true {
+            let tuple_val = testing_option.unwrap();
+            let default_order = tuple_val.1;
+            let tx_ids = default_order.iter().map(|obj| obj.compute_txid());
+            let merkel_root = TxMerkleNode::calculate_root(tx_ids);
+            println!("Merkel path proof ---- {:?}", merkel_root);
+            return merkel_root.unwrap();
         }
-
+        //transactionid along with its sibling
+        //taking hash and converting it to its raw bytes
+        let irreversed_id = self.transaction_hash.to_string();
+        let reversed_curr_tx_id = reverse_hex_byte_order(&irreversed_id);
+        let mut current_hash = [0u8; 32];
+        let result = hex::decode_to_slice(reversed_curr_tx_id, &mut current_hash);
+        match result {
+            Ok(val) => {
+                println!("Decoded successfully");
+            }
+            Err(error) => {
+                println!("{:?} this error occurred while decoding", error);
+            }
+        }
+        //traversing its siblings
+        for merkle_node in &self.merkle_path {
+            //sibling byte along the merkel path
+            let merkle_tx_hex_str = merkle_node.to_string();
+            let curr_hash_hex_str =
+                current_hash.to_hex_string(bitcoin::secp256k1::hashes::hex::Case::Lower);
+            //concatanate siblings along the merkel path
+            let concatanate_hash = format!("{curr_hash_hex_str}{merkle_tx_hex_str}");
+            let hashed_val = hash_data(&concatanate_hash);
+            hex::decode_to_slice(hashed_val, &mut current_hash)
+                .expect("An error occurred while decoding");
+        }
         TxMerkleNode::from_byte_array(current_hash)
     }
 }
--- braidpool-primitives/src/utils/test_utils.rs
+++ braidpool-primitives/src/utils/test_utils.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 use crate::beads::{Bead, TransactionWithMerklePath};
-use crate::utils::BeadHash;
 use crate::utils::bitcoin::MerklePathProof;
+use crate::utils::BeadHash;
 use ::bitcoin::absolute::LockTime;
 use ::bitcoin::absolute::Time;
 use ::bitcoin::hex::FromHex;
@@ -10,6 +10,11 @@ use ::bitcoin::{
     BlockHash, BlockHeader, BlockTime, BlockVersion, CompactTarget, OutPoint, ScriptBuf, Sequence,
     Transaction, TransactionVersion, TxIn, TxMerkleNode, TxOut, Witness,
 };
+use bitcoin::hashes::sha256d::hash;
+use bitcoin::hex::DisplayHex;
+use bitcoin::merkle_tree::MerkleNode;
+use bitcoin::transaction;
+use sha2::{Digest, Sha256};
 use std::collections::HashSet;
 pub fn create_block_header(
     version: i32,
@@ -20,12 +25,33 @@ pub fn create_block_header(
     nonce: u32,
 ) -> BlockHeader {
     BlockHeader {
-        version: BlockVersion::from_consensus(version),
+        version: BlockVersion::TWO,
         prev_blockhash: BlockHash::from_byte_array(prev_blockhash_bytes),
         merkle_root: TxMerkleNode::from_byte_array(merkle_root_bytes),
         time: BlockTime::from_u32(time),
         bits: CompactTarget::from_consensus(bits),
         nonce,
+    }
+}
+pub fn create_dummy_coinbase_transaction(
+    vout: u32,
+    sequence: u32,
+    witness_items: Vec<Vec<u8>>,
+    value: u64,
+) -> Transaction {
+    Transaction {
+        version: TransactionVersion::TWO,
+        lock_time: LockTime::ZERO,
+        input: vec![TxIn {
+            previous_output: OutPoint::COINBASE_PREVOUT,
+            script_sig: ScriptBuf::new(),
+            sequence: Sequence(sequence),
+            witness: Witness::from_slice(&witness_items),
+        }],
+        output: vec![TxOut {
+            value: Amount::from_sat(value).unwrap(),
+            script_pubkey: ScriptBuf::new(),
+        }],
     }
 }
 
@@ -54,10 +80,63 @@ pub fn create_dummy_transaction(
         }],
     }
 }
-pub fn create_dummy_merkle_path_proof(bytes: [u8; 32]) -> MerklePathProof {
+pub fn hash_data(data: &String) -> String {
+    let bytes = hex::decode(data).expect("Invalid hex string");
+
+    let mut hasher = Sha256::new();
+    hasher.update(&bytes);
+    let result = hasher.finalize();
+    let first_hash = result.to_hex_string(bitcoin::hex::Case::Lower);
+    let mut hasher_2 = Sha256::new();
+    let bytes_2 = hex::decode(first_hash).expect("sadfgh");
+    hasher_2.update(bytes_2);
+    let final_result = hasher_2.finalize();
+    final_result.to_hex_string(bitcoin::hex::Case::Lower)
+}
+pub fn reverse_hex_byte_order(hex_str: &str) -> String {
+    hex_str
+        .as_bytes()
+        .chunks(2)
+        .map(|pair| std::str::from_utf8(pair).unwrap())
+        .collect::<Vec<_>>()
+        .into_iter()
+        .rev()
+        .collect::<String>()
+}
+fn merkle_tree(leaves: Vec<String>) -> String {
+    //reversing the bytes even though not needed as sample txids are taken from explorer hence are reversed in nature
+    let mut layer: Vec<String> = leaves
+        .iter()
+        .map(|str| reverse_hex_byte_order(str))
+        .collect();
+    // let mut layer = leaves;
+    while layer.len() > 1 {
+        if layer.len() % 2 != 0 {
+            layer.push(layer.last().unwrap().clone());
+        }
+
+        let mut new_layer: Vec<String> = Vec::new();
+        for i in (0..layer.len()).step_by(2) {
+            let combined = format!("{}{}", layer[i], layer[i + 1]);
+            new_layer.push(hash_data(&combined));
+        }
+
+        layer = new_layer;
+    }
+
+    layer[0].clone()
+}
+pub fn create_dummy_merkle_path_proof(
+    transaction_id: Txid,
+    siblings: Vec<[u8; 32]>,
+) -> MerklePathProof {
+    let mut converted_vector: Vec<TxMerkleNode> = Vec::new();
+    for sibling in 0..siblings.len() {
+        converted_vector.push(TxMerkleNode::from_byte_array(siblings[sibling]));
+    }
     MerklePathProof {
-        transaction_hash: Txid::from_byte_array(bytes),
-        merkle_path: vec![],
+        transaction_hash: transaction_id,
+        merkle_path: converted_vector,
     }
 }
 pub fn create_parents_set(parents_vec: Vec<(BeadHash, Time)>) -> HashSet<(BeadHash, Time)> {
@@ -66,84 +145,146 @@ pub fn create_parents_set(parents_vec: Vec<(BeadHash, Time)>) -> HashSet<(BeadHa
 pub fn create_test_bead(
     block_version: i32,
     prev_blockhash_bytes: [u8; 32],
-    merkle_root_bytes: [u8; 32],
     timestamp: u32,
     difficulty_bits: u32,
     nonce: u32,
-    bead_hash_bytes: [u8; 32],
-    coinbase_tx_bytes: [u8; 32],
-    payout_tx_bytes: [u8; 32],
+    payout_txin_bytes: [u8; 32],
     lesser_difficulty_target: u32,
     parent_hashes: Vec<(BeadHash, Time)>,
     transactions_bytes: Vec<[u8; 32]>,
     bits: u32,
-    bead_bytes: [u8; 32],
     observed_time_at_node: u32,
 ) -> Bead {
     let target: CompactTarget = CompactTarget::from_consensus(lesser_difficulty_target);
-    let bytes: [u8; 32] = bead_bytes;
-    let beadhash: BeadHash = BlockHash::from_byte_array(bytes);
-    let blockheader: BlockHeader = create_block_header(
-        block_version,
-        prev_blockhash_bytes,
-        merkle_root_bytes,
-        timestamp,
-        bits,
-        nonce,
+    let payout_transaction =
+        create_dummy_transaction(payout_txin_bytes, 2, 0xFFFFFFFF, vec![], 10_000_000);
+    let payout_txid = payout_transaction.compute_txid().to_string();
+    let mut payout_transaction_bytes = [0u8; 32];
+    let mut coinbase_transaction_bytes = [0u8; 32];
+    let result = hex::decode_to_slice(
+        payout_txid.to_string(),
+        &mut payout_transaction_bytes as &mut [u8],
     );
-
-    let coinbase_transaction: TransactionWithMerklePath = (
-        create_dummy_transaction(
-            coinbase_tx_bytes,
-            2,
-            0xFFFFFFFF,
-            vec![
-                hex::decode("03d2e15674941bad4a996372cb87e1856d3652606d98562fe39c5e9e7e413f2105")
-                    .unwrap(),
-                hex::decode("000000").unwrap(),
-            ],
-            10_000_000,
-        ),
-        create_dummy_merkle_path_proof(coinbase_tx_bytes),
+    match result {
+        Ok(val) => {
+            println!("Successfully decoded {:?}", val);
+        }
+        Err(e) => {
+            println!(
+                "{:?} this error occurred while decoding the corresponding bytes",
+                e
+            );
+        }
+    }
+    let coinbase_transaction = create_dummy_coinbase_transaction(2, 0xFFFFFFFF, vec![], 10_000_000);
+    let result = hex::decode_to_slice(
+        coinbase_transaction.compute_txid().to_string(),
+        &mut coinbase_transaction_bytes,
     );
-
+    match result {
+        Ok(val) => {
+            println!("Successfully decoded {:?}", val);
+        }
+        Err(e) => {
+            println!(
+                "{:?} this error occurred while decoding the corresponding bytes",
+                e
+            );
+        }
+    }
+    let payout_tx_reference = payout_transaction.clone();
+    let payout_tx_id = payout_tx_reference.compute_txid();
     let parents: HashSet<(BeadHash, Time)> = create_parents_set(parent_hashes);
-
-    let mut transactions_of_bead: Vec<Transaction> = Vec::new();
-
+    //the input param of transaction bytes will be of txid paying to the transaction used in
+    //dummy transaction construction
+    let mut transactions_of_bead = Vec::new();
     for transaction in transactions_bytes.iter() {
         transactions_of_bead.push(create_dummy_transaction(
             *transaction,
             2,
             0xFFFFFFFF,
-            vec![
-                hex::decode("03d2e15674941bad4a996372cb87e1856d3652606d98562fe39c5e9e7e413f2105")
-                    .unwrap(),
-                hex::decode("000000").unwrap(),
-            ],
+            vec![],
             10_000_000,
         ));
     }
-    let payout_transaction: TransactionWithMerklePath = (
-        create_dummy_transaction(
-            payout_tx_bytes,
-            2,
-            0xFFFFFFFF,
-            vec![
-                hex::decode("03d2e15674941bad4a996372cb87e1856d3652606d98562fe39c5e9e7e413f2105")
-                    .unwrap(),
-                hex::decode("000000").unwrap(),
-            ],
-            10_000_000,
-        ),
-        create_dummy_merkle_path_proof(payout_tx_bytes),
+
+    let transaction_of_beads_reference = transactions_of_bead.clone();
+    let mut beads_txs: Vec<[u8; 32]> = Vec::new();
+    for bead_tx in transaction_of_beads_reference {
+        let curr_txid = bead_tx.compute_txid();
+        let mut curr_tx_bytes = [0u8; 32];
+        let result = hex::decode_to_slice(curr_txid.to_string(), &mut curr_tx_bytes);
+        match result {
+            Ok(val) => {
+                beads_txs.push(curr_tx_bytes);
+            }
+            Err(e) => {
+                println!(
+                    "{:?} this error occurred while decoding the corresponding bytes",
+                    e
+                );
+            }
+        }
+    }
+    let iter_ref = beads_txs.clone();
+    let mut payout_siblings = Vec::new();
+    payout_siblings.push(coinbase_transaction_bytes);
+    payout_siblings.extend(iter_ref.iter());
+    let mut coinbase_siblings = Vec::new();
+    coinbase_siblings.push(payout_transaction_bytes);
+    coinbase_siblings.extend(iter_ref.iter());
+
+    let payout_transaction_with_merkel_path: TransactionWithMerklePath = (
+        payout_tx_reference,
+        create_dummy_merkle_path_proof(payout_tx_id, payout_siblings),
     );
+    let coinbase_txid = coinbase_transaction.compute_txid();
+    let coinbase_transaction_with_merkel_path: TransactionWithMerklePath = (
+        coinbase_transaction,
+        create_dummy_merkle_path_proof(coinbase_txid, coinbase_siblings),
+    );
+
+    let mut total_txs = Vec::new();
+
+    total_txs.push(coinbase_txid.to_string());
+    total_txs.push(payout_txid.to_string());
+
+    for txs in beads_txs {
+        total_txs.push(txs.to_hex_string(bitcoin::hex::Case::Lower));
+    }
+
+    let merkel_node = merkle_tree(total_txs);
+    let mut merkel_node_bytes = [0u8; 32];
+    let result = hex::decode_to_slice(merkel_node, &mut merkel_node_bytes);
+    match result {
+        Ok(val) => {
+            println!("Successfully decoded {:?}", val);
+        }
+        Err(e) => {
+            println!(
+                "{:?} this error occurred while decoding the corresponding bytes",
+                e
+            );
+        }
+    }
+
     let time = Time::from_consensus(1653195600).expect("Invalid time");
+    let blockheader: BlockHeader = create_block_header(
+        block_version,
+        prev_blockhash_bytes,
+        merkel_node_bytes,
+        timestamp,
+        bits,
+        nonce,
+    );
+    //bead hash value taking random 32 byte hash but same as that of the block header bytes
+
+    let beadhash = blockheader.block_hash();
     return Bead {
         block_header: blockheader,
         bead_hash: beadhash,
-        coinbase_transaction: coinbase_transaction,
-        payout_update_transaction: payout_transaction,
+        coinbase_transaction: coinbase_transaction_with_merkel_path,
+        payout_update_transaction: payout_transaction_with_merkel_path,
         lesser_difficulty_target: target,
         parents: parents,
         transactions: transactions_of_bead,
