510d99d711e9d2d7ad4709470230086bc94e775c
priyashuu <priyaaashu4@gmail.com>
2025-04-23 14:49:03 +0530

    refactor: Improve ActionIconButton styles and ensure icon scales correctly

--- dashboard/src/components/common/ActionIconButton.tsx
+++ dashboard/src/components/common/ActionIconButton.tsx
@@ -5,6 +5,7 @@ interface ActionIconButtonProps {
   onClick?: () => void;
   className?: string;
 }
+
 const ActionIconButton: React.FC<ActionIconButtonProps> = ({
   icon,
   onClick,
@@ -13,9 +14,15 @@ const ActionIconButton: React.FC<ActionIconButtonProps> = ({
   return (
     <button
       onClick={onClick}
-      className={`w-[34px] h-[34px] p-1 text-white bg-[#36454F] shadow-sm hover:bg-black hover:shadow-md rounded-full ${className}`}
+      className={`
+        w-[34px] h-[34px] p-1 text-white bg-[#36454F]
+        shadow-sm hover:bg-black hover:shadow-md rounded-full
+        sm:w-[34px] sm:h-[34px]    
+        xs:w-[28px] xs:h-[28px]    
+        ${className}
+      `}
     >
-      {React.cloneElement(icon, { className: 'text-inherit' })}
+      {React.cloneElement(icon, { className: 'text-inherit w-full h-full' })}
     </button>
   );
 };
