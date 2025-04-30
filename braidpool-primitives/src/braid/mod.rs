bb357b7db1ee52f573799b68d70422b0e7705eba
Aritra Majumder <aritramajumder8438@gmail.com>
2025-04-30 18:00:41 +0530

    Add `extend` function
    Signed-off-by: Aritra Majumder <aritramajumder8438@gmail.com>

--- braidpool-primitives/src/braid/mod.rs
+++ braidpool-primitives/src/braid/mod.rs
@@ -153,6 +153,72 @@ impl Braid {
         }
     }
 
+    /// Attempts to extend the braid with the given bead.
+    /// Returns true if the bead successfully extended the braid, false otherwise.
+    fn extend(&mut self, bead: &Bead) -> bool {
+        // No parents: bad block
+        if bead.committed_metadata.parents.is_empty() {
+            return false;
+        }
+        // Don't have all parents
+        if !bead.committed_metadata.parents.iter().all(|p| self.beads.contains(p)) {
+            return false;
+        }
+        // Already seen this bead
+        let bead_hash = bead.get_bead_hash();
+        if self.beads.contains(&bead_hash) {
+            return false;
+        }
+
+        // Insert bead hash into beads set
+        self.beads.insert(bead_hash);
+
+        // Remove parents from tips if present
+        for parent in &bead.committed_metadata.parents {
+            self.tips.remove(parent);
+        }
+        // Add bead hash to tips
+        self.tips.insert(bead_hash);
+
+        // Find earliest parent of bead in cohorts and nuke all cohorts after that
+        let mut found_parents = HashSet::new();
+        let mut dangling = HashSet::new();
+        dangling.insert(bead_hash);
+
+        // We'll collect the indices to remove from cohorts
+        let mut remove_after = None;
+        for (i, cohort) in self.cohorts.iter().enumerate().rev() {
+            // Find which parents are in this cohort
+            for parent in &bead.committed_metadata.parents {
+                if cohort.0.contains(parent) {
+                    found_parents.insert(*parent);
+                }
+            }
+            // Add all bead hashes in this cohort to dangling
+            for h in &cohort.0 {
+                dangling.insert(*h);
+            }
+            if found_parents.len() == bead.committed_metadata.parents.len() {
+                remove_after = Some(i + 1);
+                break;
+            }
+        }
+        // Remove all cohorts after the found index
+        if let Some(idx) = remove_after {
+            self.cohorts.truncate(idx);
+        } else {
+            self.cohorts.clear();
+        }
+
+        // Construct a sub-braid from dangling and compute any new cohorts
+        // Here, we just create a new cohort with dangling beads
+        if !dangling.is_empty() {
+            self.cohorts.push(Cohort(dangling));
+        }
+
+        true
+    }
+
     // All private functions go here!
     fn calculate_cohorts(&self) -> Vec<Cohort> {
         // TODO: Implement the cohorts calculating function!
