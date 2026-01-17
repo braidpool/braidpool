9df75862ba07b5ca8b77bb11f612abd97383b4a0
priyashuu <priyaaashu4@gmail.com>
2026-01-18 03:04:18 +0530

    Refactor CSS import syntax and remove unnecessary whitespace in index.tsx

--- dashboard/src/index.tsx
+++ dashboard/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
