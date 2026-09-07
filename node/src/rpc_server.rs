b6e456f2e0b68cb7d5e1f403e607be56df9e427d
priyashuu <priyaaashu4@gmail.com>
2026-09-07 23:39:12 +0530

    fix(rpc): reorder imports for bitcoin::Transaction and bitcoin::Txid for consistency

--- node/src/rpc_server.rs
+++ node/src/rpc_server.rs
@@ -13,10 +13,10 @@ use crate::stratum::BlockTemplate;
 #[cfg(test)]
 use crate::utils::compute_block_hash;
 use crate::utils::BeadHash;
-use bitcoin::Transaction;
-use bitcoin::Txid;
 #[cfg(test)]
 use bitcoin::hashes::Hash;
+use bitcoin::Transaction;
+use bitcoin::Txid;
 use futures::lock::Mutex;
 use jsonrpsee::core::async_trait;
 use jsonrpsee::core::middleware::Batch;
